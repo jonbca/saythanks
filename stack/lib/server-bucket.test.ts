b94b3f10ef2d47bf413be5c0476542f8aa77bdb2
Jonathan Abourbih <jon.abourbih@gmail.com>
2019-04-06 18:32:45 +0100

    ensure bucket policy allows public access

--- stack/lib/server-bucket.test.ts
+++ stack/lib/server-bucket.test.ts
@@ -21,4 +21,30 @@ test('it has public read permissions on the bucket', () => {
     const stack = new cdk.Stack();
 
     new ServerBucket(stack, 'StackBucket');
+
+    expect(stack).to(
+        haveResource('AWS::S3::BucketPolicy', {
+            PolicyDocument: {
+                Statement: [
+                    {
+                        Action: 's3:GetObject',
+                        Effect: 'Allow',
+                        Principal: '*',
+                        Resource: {
+                            'Fn::Join': [
+                                '',
+                                [
+                                    {
+                                        'Fn::GetAtt': ['StackBucketDeploymentBucketA8E9618A', 'Arn']
+                                    },
+                                    '/*'
+                                ]
+                            ]
+                        }
+                    }
+                ],
+                Version: '2012-10-17'
+            }
+        })
+    );
 });
