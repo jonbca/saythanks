efcb61990159333d4a528ed43898fab34e896bf9
Jonathan Abourbih <jon.abourbih@gmail.com>
2019-04-07 12:25:33 +0100

    remove unused properties

--- stack/lib/server-bucket.ts
+++ stack/lib/server-bucket.ts
@@ -3,9 +3,8 @@ import s3 = require('@aws-cdk/aws-s3');
 
 export default class ServerBucket extends cdk.Construct {
     public readonly bucket: s3.Bucket;
-    public readonly websiteUrl: string;
     public readonly bucketName: string;
-    public readonly bucketUrl: string;
+    public readonly bucketArn: string;
 
     public constructor(scope: cdk.Construct, id: string) {
         super(scope, id);
@@ -16,9 +15,8 @@ export default class ServerBucket extends cdk.Construct {
             publicReadAccess: true
         });
 
-        this.websiteUrl = this.bucket.bucketWebsiteUrl;
         this.bucketName = this.bucket.bucketName;
-        this.bucketUrl = this.bucket.bucketUrl;
+        this.bucketArn = this.bucket.bucketArn;
 
         this.bucket.export();
     }
