4f34003b7be132a1bdd512edcd78c5ba13308d67
Jonathan Abourbih <jon.abourbih@gmail.com>
2019-04-15 21:13:48 +0100

    add styled-components

--- client/src/Grid.tsx
+++ client/src/Grid.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, PureComponent } from 'react';
+import styled from 'styled-components';
 import Card from './Card';
 import './Grid.css';
 import crypto from 'crypto';
@@ -22,29 +23,57 @@ function generateId({ toName, fromName, timestamp, message }: ThankYou): string 
         .digest('hex');
 }
 
+const GridBox = styled.div`
+    display: grid;
+    grid-template-columns: 100vw;
+    grid-template-rows: 10vh calc(90vh - 1rem);
+`;
+
+const TitleBox = styled.header`
+    padding-left: 1rem;
+`;
+
+const Title = styled.h1``;
+
+const Cards = styled.ul`
+    display: grid;
+    grid-gap: 1rem;
+    align-items: stretch;
+    justify-content: center;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: repeat(4, 1fr);
+    height: 100%;
+    margin: 0;
+    padding: 0 1rem 0 1rem;
+    list-style-type: none;
+`;
+
+const CardItem = styled.li`
+    background: cyan;
+    border-radius: 10px;
+`;
+
+function renderThankYou(thankYou: ThankYou): ReactNode {
+    return (
+        <CardItem key={generateId(thankYou)}>
+            <Card {...thankYou}/>
+        </CardItem>
+    );
+}
+
 class Grid extends PureComponent<GridProps> {
     public static defaultProps = {
         title: 'Thank-you!',
     };
 
-    private renderThankYou(thankYou: ThankYou): ReactNode {
-        return (
-            <li key={generateId(thankYou)} style={{ background: '#23ce45' }}>
-                <Card {...thankYou} />
-            </li>
-        );
-    }
-
     public render(): ReactNode {
         return (
-            <div className="thankyou-grid">
-                <header className="title-box">
-                    <h1>{this.props.title}</h1>
-                </header>
-                <div className="card-box">
-                    <ul>{this.props.thanks.map(this.renderThankYou)}</ul>
-                </div>
-            </div>
+            <GridBox>
+                <TitleBox>
+                    <Title>{this.props.title}</Title>
+                </TitleBox>
+                <Cards>{this.props.thanks.map(renderThankYou)}</Cards>
+            </GridBox>
         );
     }
 }
